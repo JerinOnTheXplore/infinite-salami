86af885307e0fa87592237d866f74560eb157b5b
JerinXplore <jerinjerin101325@gmail.com>
2025-03-29 17:59:47 +0600

    added salami machine

---
+++ script/login.js
@@ -0,0 +1,4 @@
+document.getElementById('add-money-box').addEventListener('click',function(event){
+    event.preventDefault();
+    window.location.href = "./welcomeindex.html" ; 
+})
---
+++ script/toggle.js
@@ -0,0 +1,8 @@
+document.getElementById('salami').style.display="none";
+document.getElementById('add-money-btn').addEventListener('click',function(){
+    document.getElementById('salami').style.display="block";
+})
+document.getElementById('add-money-btn').addEventListener('click',function(event){
+    
+    document.getElementById('scroll').scrollIntoView({behavior:"smooth"});
+})
